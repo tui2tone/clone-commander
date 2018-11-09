90b76cd1498c0fbb736e32a1766e433137134e60
tui2tone <tui2tone@gmail.com>
2018-11-10 00:20:52 +0700

    [+] initialize project with clone command

---
+++ index.js
@@ -0,0 +1,21 @@
+const program = require('commander');
+const Lib = require('./lib/clone');
+
+function range(val) {
+    return val.split(':').map(String);
+}
+
+program
+    .version('0.0.1', '-v, --version')
+    .option('-f, --from [value]', 'A start folder to clone')
+    .option('-t, --to [value]', 'A destination folder')
+    .option('-r, --replace <a>..<b>', 'A replace command', range)
+    .parse(process.argv);
+
+
+const start = () => {
+    const { from, to, replace } = program;
+    Lib.clone(from, to, replace)
+}
+
+start();
